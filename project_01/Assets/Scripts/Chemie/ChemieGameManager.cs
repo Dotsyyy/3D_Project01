c49cd2e40bc12c92cde959cc5b3e35f40dce46ab
Dotsyyy <jordyneef7@gmail.com>
2024-11-04 12:35:34 +0100

    Update 2 WaterSort

--- project_01/Assets/Scripts/Chemie/ChemieGameManager.cs
+++ project_01/Assets/Scripts/Chemie/ChemieGameManager.cs
@@ -1,7 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class ChemieGameManager : MonoBehaviour
 {
     public List<Bottle> bottles; // List of all bottles in the game
 
