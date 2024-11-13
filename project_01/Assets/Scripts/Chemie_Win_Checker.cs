da5695b780012a7b4bc388d7761b6686d1ed0cf0
Dotsyyy <jordyneef7@gmail.com>
2024-11-13 14:43:41 +0100

    small update for winning chemie
    aaa

---
+++ project_01/Assets/Scripts/Chemie_Win_Checker.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chemie_Win_Checker : MonoBehaviour
+{
+    public Animator anim;
+    private Renderer paperRenderer;
+    public Material winMaterial;
+    bool inTrigger = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger && paperRenderer.material.color == winMaterial.color)
+        {
+            anim.SetBool("youWon", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object entering is the small cube (tagged "SmallCube")
+        if (other.CompareTag("PHTag"))
+        {
+            // Get the Renderer of the small cube to change its material
+            paperRenderer = other.GetComponent<Renderer>();
+            inTrigger = true;
+        }
+    }
+}
