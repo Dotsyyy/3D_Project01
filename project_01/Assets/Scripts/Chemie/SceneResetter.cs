05cc9bbf54e14a58806c6ad2b62cd33b129063dc
Dotsyyy <jordyneef7@gmail.com>
2024-12-15 14:32:02 +0100

    Nieuwe button

--- project_01/Assets/Scripts/Chemie/SceneResetter.cs
+++ project_01/Assets/Scripts/Chemie/SceneResetter.cs
@@ -6,10 +6,6 @@ public class SceneResetter : MonoBehaviour
     // Methode om de huidige scene opnieuw te laden
     public void ResetScene()
     {
-        // Haal de actieve scene op
-        Scene currentScene = SceneManager.GetActiveScene();
-
-        // Herlaad de actieve scene
-        SceneManager.LoadScene(currentScene.name);
+        SceneManager.LoadScene("Chemie");
     }
 }
