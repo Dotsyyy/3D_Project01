0ffe2a5bc2b5176a3529e4110fa66f3b371ff512
Dotsyyy <jordyneef7@gmail.com>
2024-12-30 10:53:10 +0100

    Revert "Eindelijk klaar met deze shit game"
    This reverts commit c13852544d4d7140a73fbc6dee097c66d5f0a100.

--- project_01/Assets/Scripts/BuildingBStart.cs
+++ project_01/Assets/Scripts/BuildingBStart.cs
@@ -7,8 +7,6 @@ public class BuildingBStart : MonoBehaviour
     public GameObject buildingB;
     public string sceneName = "Chemie";
     public GameObject outlineBuilding;
-    public Animator animator;
-    private bool isPressed = false;
 
     void Update()
     {
@@ -40,30 +38,8 @@ public class BuildingBStart : MonoBehaviour
     public void GebouwClicked()
     {
         if (!CanProceed()) return;
-        if (isPressed == false)
-        {
-            buildingB.SetActive(true);
-            outlineBuilding.SetActive(true);
-            animator.SetBool("Move", true);
-        }
-        if (isPressed == true)
-        {
-            animator.SetBool("Small", true);
-        }
-        
-    }
 
-    public void ChangeBool()
-    {
-        isPressed = true;
-    }
-
-    public void WhenEvent()
-    {
-        outlineBuilding.SetActive(false);
-        isPressed = false;
-        animator.SetBool("Move", false);
-        animator.SetBool("Small", false);
-        buildingB.SetActive(false);
+        buildingB.SetActive(true);
+        outlineBuilding.SetActive(true);
     }
 }
